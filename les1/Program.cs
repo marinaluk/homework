6af6036c8fede62cb566d0981b077be5a5354f7e
unknown <C:\Users\Marina\AppData\Roaming\The Bat!>
2022-11-25 23:01:02 +0800

    сделали задачу 2

---
+++ les1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("input number 1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number 2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+    if (num1 > max ) max = num1;
+    if ( num2 > max) max = num2;
+
+Console.WriteLine(max);
